ce7bf7e5331e2ee67e9e32502b369c0fddcb4b1e
PauloAlves8039 <pj38alves@gmail.com>
2021-05-03 10:30:24 -0300

    Aplicando os conteúdos das telas

--- lib/TelaCliente.dart
+++ lib/TelaCliente.dart
@@ -8,6 +8,46 @@ class TelaCliente extends StatefulWidget {
 class _TelaClienteState extends State<TelaCliente> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Clientes"),
+        backgroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Image.asset("assets/imagens/detalhe_cliente.png"),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Clientes",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Image.asset("assets/imagens/cliente1.png"),
+              ),
+              Text("Empresa de software"),
+              Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Image.asset("assets/imagens/cliente2.png"),
+              ),
+              Text("Empresa de auditoria"),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/TelaEmpresa.dart
+++ lib/TelaEmpresa.dart
@@ -8,6 +8,61 @@ class TelaEmpresa extends StatefulWidget {
 class _TelaEmpresaState extends State<TelaEmpresa> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Empresa"),
+        backgroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Image.asset("assets/imagens/detalhe_empresa.png"),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Sobre a empresa",
+                      style: TextStyle(fontSize: 20, color: Colors.deepOrange),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+                    "when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+                    "It has survived not only five centuries, but also the leap into electronic typesetting,"
+                    "remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,"
+                    "and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\n"
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+                    "when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+                    "It has survived not only five centuries, but also the leap into electronic typesetting,"
+                    "remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,"
+                    "and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\n"
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+                    "when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+                    "It has survived not only five centuries, but also the leap into electronic typesetting,"
+                    "remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,"
+                    "and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\n"
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+                    "when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+                    "It has survived not only five centuries, but also the leap into electronic typesetting,"
+                    "remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,"
+                    "and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\n"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
